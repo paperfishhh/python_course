dd7910f096b4f168dea277ea7a70fdec69203177
paperfishhh <bnini123456789@gmail.com>
2023-10-06 09:33:30 +0800

    Create s1100663_Paperfishhh_world.py
    1100663

---
+++ students/w1/s1100663_Paperfishhh_world.py
@@ -0,0 +1,16 @@
+import sys;
+
+# hello world 
+
+# print hellowrld with variable 
+def hello():
+    print('1100663 李芷榆 ')
+
+
+def showpythonversion():
+    print('python version is :', sys.version)
+
+# call hello function    
+
+hello()
+showpythonversion()
