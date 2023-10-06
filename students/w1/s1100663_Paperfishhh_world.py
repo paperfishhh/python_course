d3d492f08254cd13ee05ba21f0eb1bba2ebff70d
paperfishhh <bnini123456789@gmail.com>
2023-10-06 10:06:37 +0800

    Update s1100663_Paperfishhh_world.py
    s1100663

--- students/w1/s1100663_Paperfishhh_world.py
+++ students/w1/s1100663_Paperfishhh_world.py
@@ -3,14 +3,7 @@ import sys;
 # hello world 
 
 # print hellowrld with variable 
-def hello():
-    print('1100663 李芷榆 ')
+def hello(id,name):
+    print(id,name)
 
-
-def showpythonversion():
-    print('python version is :', sys.version)
-
-# call hello function    
-
-hello()
-showpythonversion()
+hello(1100663,"李芷榆")
